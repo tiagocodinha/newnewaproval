74439fc0d6fa008f0006056ea272bede6ef14b10
tiagocodinha <98787370+tiagocodinha@users.noreply.github.com>
2025-03-11 22:58:45 +0000

    Revert "Update main.tsx"
    This reverts commit fb228f3aabf04a27e948a7cd1f19a58a35b2064d.

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
 import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <GoogleReCaptchaProvider
-      reCaptchaKey="6LeYnfEqAAAAPEXq1ju-2NEDO5jNdVS7pmUfsvz"
-      scriptProps={{
-        async: false,
-        defer: false,
-        appendTo: 'head',
-        nonce: undefined,
-      }}
-    >
-      <App />
-    </GoogleReCaptchaProvider>
+    <App />
   </StrictMode>
 );
