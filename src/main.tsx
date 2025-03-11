fb228f3aabf04a27e948a7cd1f19a58a35b2064d
tiagocodinha <98787370+tiagocodinha@users.noreply.github.com>
2025-03-11 22:49:01 +0000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
 import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <GoogleReCaptchaProvider
+      reCaptchaKey="6LeYnfEqAAAAPEXq1ju-2NEDO5jNdVS7pmUfsvz"
+      scriptProps={{
+        async: false,
+        defer: false,
+        appendTo: 'head',
+        nonce: undefined,
+      }}
+    >
+      <App />
+    </GoogleReCaptchaProvider>
   </StrictMode>
 );
